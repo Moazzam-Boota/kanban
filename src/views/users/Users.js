ce0e6e979e08a5087541b8d2ce69f8ab7ccfcd57
AdnanBoota <inshallah_1@yahoo.com>
2021-04-27 18:58:06 +0200

    improvements

--- src/views/users/Users.js
+++ src/views/users/Users.js
@@ -31,6 +31,7 @@ const Users = () => {
     //   socket.emit("lightred", Number(this.checked)); //send button status to server (as 1 or 0)
     // });
     // });
+    console.log('use effect - green');
     socket.on('lightgreen', function (data) { //get button status from client
       // document.getElementById("lightgreen").checked = data; //change checkbox according to push button on Raspberry Pi
       socket.emit("lightgreen", data); //send push button status to back to server
