75663917f9391566d5b6af452cb4886433942ada
AdnanBoota <inshallah_1@yahoo.com>
2021-05-16 17:48:29 +0200

    update

--- src/views/Chart/Chart.js
+++ src/views/Chart/Chart.js
@@ -247,23 +247,21 @@ const Users = () => {
   }, [parentsData]);
 
   // var oldShiftsRemainder = 0;
-  var localDonePieces = 0;
   useEffect(() => {
     // console.log(donePieces, 'donePieces')
-
     const allShiftsData = [...dataGroupByProduct];
     var allShiftsDataLength = lodash.get(allShiftsData, '[0].length', 0);
     var allShiftsDataRemainder = lodash.get(allShiftsData, [[0], [allShiftsDataLength - 1], 'originalCount'], 0);
     // oldShiftsRemainder += allShiftsDataRemainder;
+    var localDonePieces = donePieces;
     var shiftPieceDoneLimit = localDonePieces % (allShiftsDataRemainder + 1);
 
     console.log('updatedShiftData2', shiftPieceDoneLimit, allShiftsDataRemainder)
     if (allShiftsData[0] && allShiftsData[0][allShiftsData[0].length - 1].originalCount - shiftPieceDoneLimit > 0) {
       allShiftsData[0][allShiftsData[0].length - 1].productCount = allShiftsData[0][allShiftsData[0].length - 1].originalCount - shiftPieceDoneLimit;
-      ++localDonePieces;
     } else if (allShiftsData[0] && allShiftsData[0][allShiftsData[0].length - 1].originalCount - shiftPieceDoneLimit <= 0) {
-      localDonePieces = 0;
       if (allShiftsData[0].length > 1) {
+        localDonePieces = 0;
         allShiftsData[0].pop();
         // allShiftsDataLength = lodash.get(allShiftsData, '[0].length', 0);
         // allShiftsDataRemainder = lodash.get(allShiftsData, [[0], [allShiftsDataLength - 1], 'originalCount'], 0);
