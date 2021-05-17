b7ee5e3eee80fc4f79e94de0bc6acc507d93f2cf
AdnanBoota <inshallah_1@yahoo.com>
2021-05-17 18:12:06 +0200

    update

--- src/views/Chart/Chart.js
+++ src/views/Chart/Chart.js
@@ -83,7 +83,7 @@ const Users = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setTimeLeft(moment());
-      // setTrackShiftsDone(trackShiftsDone - 1);
+      setTrackShiftsDone(trackShiftsDone - 1);
       cardsData = [];
       renderCards();
       // }, pitchTime * 60 * 1000);
@@ -351,7 +351,7 @@ const Users = () => {
 
       var beforeTime = moment(startShiftTime.format('HH:mm'), format);
       var afterTime = moment(startShiftTime.subtract(pitchTime, 'minutes').format('HH:mm'), format);
-      const currentPointer = i - trackShiftsDone;
+      const currentPointer = i;
       if (currentPointer <= parseInt(blueColorChartParams.min)) { color = 'blue'; }
       else if (currentPointer >= parseInt(greenColorChartParams.min) && currentPointer <= parseInt(greenColorChartParams.max)) { color = 'green'; }
       else if (currentPointer >= parseInt(orangeColorChartParams.min) && currentPointer <= parseInt(orangeColorChartParams.max)) { color = 'orange'; }
@@ -361,7 +361,7 @@ const Users = () => {
       // console.log(currentTime, afterTime, beforeTime, 'hello')
       if (currentTime.isBetween(afterTime, beforeTime)) {
         // also check for length of allShiftsData
-        activeShiftPeriod = currentPointer;
+        activeShiftPeriod = i - trackShiftsDone;
         headerWidgetColor = color;
         console.log(i - trackShiftsDone, 'here is')
       }
